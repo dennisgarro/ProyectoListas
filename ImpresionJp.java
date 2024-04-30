b2fffd1c238a993740fc8a8209e4abf8dd384ff8
Pablobf19 <juampa2045@gmail.com>
2024-04-30 18:48:35 -0500

    cambio jp

---
+++ ImpresionJp.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class ImpresionJp {
+    
+    public String Impresionjp(LinkedList<Empleados> empleados){
+    String cadena = "";
+        for (Empleados employee : empleados)
+        {
+          
+        }
+        return cadena;
+    }
+
+}
